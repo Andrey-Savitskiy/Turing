148fcd935e85b0148f56c3bd9d291b841e9a71ae
MrBlackDog <sasha29091999@yandex.ru>
2023-04-17 11:11:27 +0300

    add task description

--- Module_15.04/task_4.js
+++ Module_15.04/task_4.js
@@ -1,3 +1,11 @@
+/*
+Задача 4. "Полифил map"
+Реализуйте функцию, которая принимает на вход массив и функцию-
+обработчик callback. Возвращает новый массив, к каждому элементу 
+которого применена функция-обработчик. 
+*/
+
+
 function array_decorator(array, callback_function) {
     let result_list = array.map(callback_function)
 
